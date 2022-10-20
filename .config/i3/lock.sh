eeaa9180559332ab73be506adf4cbe6f319d1366
Bubitor <108011186+Bubitor@users.noreply.github.com>
2022-10-20 17:58:03 +0000

    Add files via upload

---
+++ .config/i3/lock.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+
+BLANK='#000000'
+CLEAR='#000000'
+DEFAULT='#1a1a1a'
+TEXT='#0000ee'
+WRONG='#880000'
+VERIFYING='#006400'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--screen 1                   \
+--blur 5                     \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M"        \
+--date-str="%d. %m. %Y"       \
+--keylayout 1                \

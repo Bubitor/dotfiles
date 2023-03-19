56318c28981b7b2ce63c1f146348042e3e413570
Bubitor <108011186+Bubitor@users.noreply.github.com>
2023-03-19 14:42:52 +0000

    Add files via upload

---
+++ .config/wal/templates/colors.sh
@@ -0,0 +1,26 @@
+# Fishs shell variables
+# set -U xx xx
+set -U wallpaper {wallpaper}
+
+# Special
+set -U background {background} 
+set -U foreground {foreground}
+set -U cursor {cursor}
+
+# Colors
+set -U color0 {color0}
+set -U color1 {color1}
+set -U color2 {color2}
+set -U color3 {color3}
+set -U color4 {color4}
+set -U color5 {color5}
+set -U color6 {color6}
+set -U color7 {color7}
+set -U color8 {color8}
+set -U color9 {color9}
+set -U color10 {color10}
+set -U color11 {color11}
+set -U color12 {color12}
+set -U color13 {color13}
+set -U color14 {color14}
+set -U color15 {color15}
